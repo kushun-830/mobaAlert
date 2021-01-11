3e71707d22d85fb526af9f26214b1320f72f28e0
kushun-830 <kubo-shunsuke830@g.ecc.u-tokyo.ac.jp>
2021-01-11 21:36:46 +0900

    ver 0.2.1

--- main.py
+++ main.py
@@ -32,12 +32,14 @@ if __name__ == '__main__':
     import requests
     import datetime
 
+    debug = False
+
     def time_import():
         #nowtime = datetime.datetime(2020,12,26,15,48,40)  #ぷちコレ #デバッグ用
         #nowtime = datetime.datetime(2020,12,31,14,59)  #イベントが存在しない時間 #開始直前 #デバッグ用
         #nowtime = datetime.datetime(2020,12,31,15,48,50)  #ドリフ開催（フィーバー）期間 #デバッグ用
         #nowtime = datetime.datetime(2020,12,31,13,20)  #ドリフ開催（非フィーバー）期間 #デバッグ用
-        #nowtime = datetime.datetime(2021,1,10,20,58,50)  #デバッグ用
+        #nowtime = datetime.datetime(2021,1,11,20,57,50)  #デバッグ用
         #nowtime = datetime.datetime(2021,1,8,20,30)  #イベント終了直前 #デバッグ用
         nowtime = datetime.datetime.now()
         return nowtime #現在時刻を返す
@@ -88,19 +90,28 @@ if __name__ == '__main__':
     window = sg.Window("モバマスイベントアラート", layout, resizable = True)
 
     #定数設定
-    timeout_time = 1000 * 10 * 3
-    roundPopup = True
+    timeout_time = 1000 * 10
+    roundPopup_settings = True
+    roundPopup = roundPopup_settings
     roundPopupTime = 10
-    #i = 0
+    isIntervel = False
+
+    if debug:
+        diff = 0
+    else:
+        timeout_time *= 3
 
     #イベント待ち
     while True:
-        #i += 0.5
+        if debug:
+            diff += 0.5
         appliEvent, appliValues = window.read(timeout = timeout_time, timeout_key = "timeout")
         print(" イベント:", appliEvent , ", 値:", appliValues)    #デバッグ用
 
         if appliEvent == "timeout":
-            nowTime = time_import() #+ datetime.timedelta(minutes = i) #デバッグ用 #iを適当に置けば定数時刻からずらしていける
+            nowTime = time_import()
+            if debug:
+                nowTime += datetime.timedelta(minutes = diff) #デバッグ用 #iを適当に置けば定数時刻からずらしていける
             updateTime = nowTime.isoformat(timespec = "seconds").replace("T", " ")
             print(nowTime)  # デバッグ用
 
@@ -124,14 +135,15 @@ if __name__ == '__main__':
                     if isRoundEnd:
                         sg.popup("ラウンド終了" + str(roundPopupTime) + "分前です")
                         roundPopup = False
-                elif isFeverOrRound != "インターバル":
-                    roundPopup = True
+
+                if isFeverOrRound == "インターバル":
+                    roundPopup = roundPopup_settings
 
                 if isEventTest.isEventEnd(nowTime, eventDataSet):
                     eventName = "インターバル期間"
                     eventDataSet = {}
                     eventFlag = False
-                    roundPopup = True
+                    roundPopup = roundPopup_settings
 
             window["eventName"].set_size(size = (len(eventName) * 3, None))
             window["eventName"].update(eventName)
